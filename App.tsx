acf24294ade8811b10e7135dc321e36f07ae7e37
Het Patel <166630707+itsssHp@users.noreply.github.com>
2025-03-05 12:48:05 -0700

    Import and calling API

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet, ActivityIndicator } from 'react-native';
+import axios from 'axios';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const API_HOST = 'numbersapi.p.rapidapi.com';
+const API_KEY = '77909bf3a8msh14651affa094e91p103f94jsnee75454521ae';
