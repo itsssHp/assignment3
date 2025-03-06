7a39285e6e8c2a63daa6de7945c7341cca581fa0
Deep Patel <166842823+Deep4724@users.noreply.github.com>
2025-03-05 22:24:15 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -130,3 +130,25 @@ const styles = StyleSheet.create({
     color: '#333',
   },
 });
+
+const pickerSelectStyles = {
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    color: 'black',
+    paddingRight: 30,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    color: 'black',
+  },
+};
